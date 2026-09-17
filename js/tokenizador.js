6a85b0d2e1a5bfe355a8ffca7072b495ac2653e1
Dadaí Alejandro Ovando Velásquez <dovandov@miumg.edu.gt>
2026-09-16 21:08:27 -0600

    Nivel 3 — Evaluador de expresiones aritméticas: Se agregó la estructura de datos y se termino el módulo del tokenizador

--- js/tokenizador.js
+++ js/tokenizador.js
@@ -17,13 +17,10 @@ export function TokenizarFormula(formula) {
             if (textoAcumulado.length > 0) {
                 cantidadTokens = cantidadTokens + 1
                 letra1 = textoAcumulado[1]
-                //TODO: Aqui se ingresaria el valor acumulado el diccionario de objetos independietes, 
-                //Pendiente definir e integrar la estructura de datos final.
                 ObtenerTipoDeDatoNumeroCeldaFuncionRango(textoAcumulado, letra1,formulaTokenizada);
                 textoAcumulado = "";
             }
             cantidadTokens = cantidadTokens + 1;
-            //TODO: Aqui se ingresaria el valor del texto temporal el diccionario de objetos independietes, 
             ObtenerTipoDeDatoParOperadorSeparadorAgrupador(textoTemporal, formulaTokenizada);
         }else{
             if (textoTemporal !== ESPACIO) {
@@ -32,27 +29,27 @@ export function TokenizarFormula(formula) {
         }
         i = i + 1;
     }
+    return formulaTokenizada;
 }
 
 function ObtenerTipoDeDatoNumeroCeldaFuncionRango(textoAcumulado, letra1,formulaTokenizada){
-//TODO: Implementar lógica de clasificación para numeros y celdas
  if (false) {
     //TODO: Aquí, iria la comprobación para saber si se trata de una función rango(PROMEDIO,MAX,MIN,SUMA)
  }else if (letra1 === "0" || letra1 === "1" || letra1 === "2" || letra1 === "3" || letra1 === "4" || letra1 === "5" || letra1 === "6" || letra1 === "7" || letra1 === "8" || letra1 === "9") {
-    //TODO: Aquí se ingresaria el tipo(Número) con su valor al arreglo formulaTokenizada como objeto independiente 
+    formulaTokenizada.push({tipo:"Numero", valor:  textoAcumulado})
  }else {
-    //TODO: Aquí se ingresaria el tipo(Celda) con su valor al arreglo formulaTokenizada como objeto independiente 
+    formulaTokenizada.push({tipo:"Celda", valor:  textoAcumulado})
  }
 }
 
 function ObtenerTipoDeDatoParOperadorSeparadorAgrupador(textoTemporal,formulaTokenizada){
     if (textoTemporal === "(" || textoTemporal === ")") {
-        //TODO: Aquí se ingresaria el tipo(Parentesis) con su valor al arreglo formulaTokenizada como objeto independiente 
+        formulaTokenizada.push({tipo:"Parentesis", valor:  textoAcumulado})
     } else if (textoTemporal === ":"){
-        //TODO: Aquí se ingresaria el tipo(OperadorRango) con su valor al arreglo formulaTokenizada como objeto independiente 
+        formulaTokenizada.push({tipo:"OperadorRango", valor:  textoAcumulado})
     }else if (textoTemporal === ";" || textoTemporal === ","){
-        //TODO: Aquí se ingresaria el tipo(Separador) con su valor al arreglo formulaTokenizada como objeto independiente 
+        formulaTokenizada.push({tipo:"Separador", valor:  textoAcumulado})
     }else {
-        //TODO: Aquí se ingresaria el tipo(Operador) con su valor al arreglo formulaTokenizada como objeto independiente
+        formulaTokenizada.push({tipo:"Operador", valor:  textoAcumulado})
     }
 }
