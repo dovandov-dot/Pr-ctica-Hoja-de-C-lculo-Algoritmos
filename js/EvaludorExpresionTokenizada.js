525ae9934ed24fe7008f15c5a6c031a9783f08ca
Dadaí Alejandro Ovando Velásquez <dovandov@miumg.edu.gt>
2026-09-18 02:23:27 -0600

    Nivel 3 — Evaluador de expresiones aritméticas: se agregó la estructura del evaluador

---
+++ js/EvaludorExpresionTokenizada.js
@@ -0,0 +1,41 @@
+export class EvaluadroAST{
+    constructor(arbolAST){
+        this.arbolAST = arbolAST;
+    }
+
+    EvaluarAST(){
+
+    }
+
+    OperarObjetoAST(){
+
+    }
+
+    OperarOperador(){
+
+    }
+
+    PasarTextoANumero(){
+
+    }
+
+    ObtenerValorCelda(){
+
+    }
+
+    VerificarSiEsNumero(){
+
+    }
+
+    RealizarOperacion(){
+
+    }
+
+    OperarDivisin(){
+
+    }
+
+    OperarPorcentaje(){
+        
+    }
+}
