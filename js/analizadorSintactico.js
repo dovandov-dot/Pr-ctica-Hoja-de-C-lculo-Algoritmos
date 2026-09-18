707b1f204845f6bd4ddafef380624bdee3b2288c
Dadaí Alejandro Ovando Velásquez <dovandov@miumg.edu.gt>
2026-09-17 22:13:28 -0600

    Nivel 3 — Evaluador de expresiones aritméticas: se modificó la estructura del analizador sintáctico

--- js/analizadorSintactico.js
+++ js/analizadorSintactico.js
@@ -1,43 +1,61 @@
-export function OperarSumaResta(formulaTokenizada,posicionToken) {
-    const arbolAST = {}
+export class AnalizadorSintactico {
+    constructor(formulaTokenizada) {
+        this.formulaTokenizada = formulaTokenizada;
+        this.posicionToken = 0;
+        this.arbolAST = {}
+    }
+
+    OperarSumaResta(formulaTokenizada, posicionToken) {
+        const arbolAST = {}
+        let numeroIzquierdo = {}
+        let numeroDerecho = {}
+        let resultado = 0
+
+        numeroIzquierdo = OperarMultiplicacionDivision(formulaTokenizada, posicionToken, arbolAST,)
+
+        while (posicionToken < formulaTokenizada.length && formulaTokenizada[posicionToken].valor === "+" || formulaTokenizada[posicionToken].valor === "-") {
+            
+        }
     
+    }
+
+    OperarMultiplicacionDivision(formulaTokenizada,posicionToken,arbolAST) {
+    
+    }
+
+    OperarPorcentaje(formulaTokenizada,posicionToken) {
+    
+    }
+
+    OperarPorcentajeFormaBinaria(formulaTokenizada,posicionToken) {
+    
+    }
+
+    OperarPorcentajeFormaUnitaria(formulaTokenizada,posicionToken) {
+    
+    }
+
+    operarSignoNegativoPositivo(formulaTokenizada,posicionToken) {
+    
+    }
+
+    operarNumeroFuncionCeldaParéntesisRango(formulaTokenizada,posicionToken) {
+    
+    }
+
+    ObtenerNumero(formulaTokenizada,posicionToken) {
+    
+    }
+
+    ObtenerCelda(formulaTokenizada,posicionToken) {
+    
+    }
+
+    OperarParentesis(formulaTokenizada,posicionToken) {
+    
+    }
 }
 
-function OperarMultiplicacionDivision(formulaTokenizada,posicionToken, ) {
-    
-}
-
-function OperarPorcentaje(formulaTokenizada,posicionToken) {
-    
-}
-
-function OperarPorcentajeFormaBinaria(formulaTokenizada,posicionToken) {
-    
-}
-
-function OperarPorcentajeFormaUnitaria(formulaTokenizada,posicionToken) {
-    
-}
-
-function operarSignoNegativoPositivo(formulaTokenizada,posicionToken) {
-    
-}
-
-function operarNumeroFuncionCeldaParéntesisRango(formulaTokenizada,posicionToken) {
-    
-}
-
-function ObtenerNumero(formulaTokenizada,posicionToken) {
-    
-}
-
-function ObtenerCelda(formulaTokenizada,posicionToken) {
-    
-}
-
-function OperarParentesis(formulaTokenizada,posicionToken) {
-    
-}
 
 //Mas adelante se agregará la lógica para las fuciones de rango 
 //function OperarFuncionRango(formulaTokenizada,posicionToken) {
